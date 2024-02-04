c11fee876de5db4e745a4d7b6ff6bf8b62ceefd7
Trevor Ingram <ingramtrevor10@gmail.com>
2024-02-03 18:52:12 -0800

    Corrected typo.

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import HornedBeast from "./HornedBeast";
 
 function Gallery() {
-  // You can set the properties with your desired values
   const gazelle = {
     title: "Gazelle",
     imageUrl: "https://pixexid.com/api/download/image/a-4k-ultra-hd-mobile-wallpaper-featuring-a-majestic-and-graceful-pronghorn-antel-jv6mql1t.jpeg",
